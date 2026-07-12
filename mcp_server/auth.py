543135e48722ef829172acc8c6b93569ef010ae1
Kartik Mathur <kartik.mathur@cubastion.com>
2026-07-13 02:06:17 +0530

    Add gmail.readonly scope to OAuth configuration

--- mcp_server/auth.py
+++ mcp_server/auth.py
@@ -10,6 +10,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SCOPES = [
     "https://www.googleapis.com/auth/documents",
     "https://www.googleapis.com/auth/gmail.compose",
+    "https://www.googleapis.com/auth/gmail.readonly",
     "https://www.googleapis.com/auth/gmail.labels"
 ]
 
